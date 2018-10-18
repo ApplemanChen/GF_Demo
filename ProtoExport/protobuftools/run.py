12bced0385b0bb1d6381c59cfc67169bf6c307d4
Chenhua <1184923569@qq.com>
2018-10-18 15:19:22 +0800

    proto 工具增加pb文件的导出，用于XLua协议解析；

--- ProtoExport/protobuftools/run.py
+++ ProtoExport/protobuftools/run.py
@@ -25,7 +25,7 @@ def export_cs():
 # 导出python文件
 def export_py():
 	global proto_path
-	protoc_path = os.getcwd()+"/protoc"
+	global protoc_path
 	
 	# protoc 生成py时，必须将当前目录改到proto所在目录，参数中路径必须用相对路径
 	os.chdir(proto_path)
@@ -37,12 +37,31 @@ def export_py():
 
 	pass
 
+# 导出pb(protobuf的二进制)文件
+def export_pb():
+	global proto_path
+	global output_path
+	global protoc_path
+
+	for file in os.listdir(proto_path):
+		if file[-6:] == ".proto" and os.path.isfile(os.path.join(proto_path,file)):
+			pb_file = os.path.join(output_path,"pb",file.replace("proto","pb")) 
+			proto_file = os.path.join(proto_path,file)
+			cmd = "{0} --proto_path={1} -o {2} {3}".format(protoc_path,proto_path,pb_file,proto_file)
+			os.system(cmd)		
+
+	pass
+
 def main():
-	global proto_path,output_path
+	global proto_path,output_path,protoc_path
 	proto_path = os.path.abspath(os.path.dirname(os.getcwd()))+"/proto/"
 	output_path = os.path.abspath(os.path.dirname(os.getcwd()))+"/output/"
-	export_cs()
-	export_py()
+	print(proto_path)
+	print(output_path)
+	protoc_path = os.getcwd()+"/protoc"
+	# export_cs()
+	# export_py()
+	export_pb()
 	pass
 
 if __name__ == '__main__':
